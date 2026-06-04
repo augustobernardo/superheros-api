ada5679a39838aca15160e64685ceb5b0606d110
augustobernardo <gutooliveira027@gmail.com>
2026-06-04 03:51:04 -0300

    feat(interceptors): add logging and transform interceptors for request/response handling

---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const { method, url } = request;
+    const start = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        const response = context.switchToHttp().getResponse<Response>();
+        const duration = Date.now() - start;
+        console.log(`${method} ${url} ${response.statusCode} - ${duration}ms`);
+      }),
+    );
+  }
+}
