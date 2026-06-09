5d84741ffa2394116a582444f8b997e7b1f0351f
augustobernardo <gutooliveira027@gmail.com>
2026-06-09 20:34:14 -0300

    fix(db): add migration for initial schema db

--- src/database/postgres/migrations/1781024814022-InitialSchema.ts
+++ src/database/postgres/migrations/1781024814022-InitialSchema.ts
@@ -1,10 +1,10 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class InitialSchema1781024814022 implements MigrationInterface {
-    name = 'InitialSchema1781024814022'
+  name = 'InitialSchema1781024814022';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TABLE "alignments" (
                 "id" SERIAL NOT NULL,
                 "name" character varying(255) NOT NULL,
@@ -12,7 +12,7 @@ export class InitialSchema1781024814022 implements MigrationInterface {
                 CONSTRAINT "PK_791b0e88f94a57e804ffe4fda4a" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "publishers" (
                 "id" SERIAL NOT NULL,
                 "name" character varying(255) NOT NULL,
@@ -20,10 +20,10 @@ export class InitialSchema1781024814022 implements MigrationInterface {
                 CONSTRAINT "PK_9d73f23749dca512efc3ccbea6a" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TYPE "public"."heroes_status_enum" AS ENUM('DRAFT', 'PUBLISHED', 'ARCHIVED')
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "heroes" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "name" character varying(255) NOT NULL,
@@ -39,7 +39,7 @@ export class InitialSchema1781024814022 implements MigrationInterface {
                 CONSTRAINT "PK_9db096e6a3c6fe87c82c0af18fc" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "attributes" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "hero_id" uuid NOT NULL,
@@ -52,10 +52,10 @@ export class InitialSchema1781024814022 implements MigrationInterface {
                 CONSTRAINT "PK_32216e2e61830211d3a5d7fa72c" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TYPE "public"."users_role_enum" AS ENUM('ADMIN', 'EDITOR', 'VIEWER')
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "users" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "cpf" character varying(11) NOT NULL,
@@ -75,7 +75,7 @@ export class InitialSchema1781024814022 implements MigrationInterface {
                 CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "revoked_tokens" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "jti" character varying(255) NOT NULL,
@@ -86,7 +86,7 @@ export class InitialSchema1781024814022 implements MigrationInterface {
                 CONSTRAINT "PK_5c2b3ed5a8f0e4972e358985038" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "powers" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "hero_id" uuid NOT NULL,
@@ -99,71 +99,70 @@ export class InitialSchema1781024814022 implements MigrationInterface {
                 CONSTRAINT "PK_5975f98852baf545611a53c0996" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "heroes"
             ADD CONSTRAINT "FK_2865df5ead1fb8f768a342676fd" FOREIGN KEY ("publisher_id") REFERENCES "publishers"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "heroes"
             ADD CONSTRAINT "FK_febd0a5e4eac8e0b9c7cf6315d3" FOREIGN KEY ("alignment_id") REFERENCES "alignments"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "attributes"
             ADD CONSTRAINT "FK_74abf75e6d975ab24659cc6b7c3" FOREIGN KEY ("hero_id") REFERENCES "heroes"("id") ON DELETE CASCADE ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "revoked_tokens"
             ADD CONSTRAINT "FK_483872b2fdc8f1ec750c9c7567c" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "powers"
             ADD CONSTRAINT "FK_aa54dbe60691f3d57a36c7edd73" FOREIGN KEY ("hero_id") REFERENCES "heroes"("id") ON DELETE CASCADE ON UPDATE NO ACTION
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "powers" DROP CONSTRAINT "FK_aa54dbe60691f3d57a36c7edd73"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "revoked_tokens" DROP CONSTRAINT "FK_483872b2fdc8f1ec750c9c7567c"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "attributes" DROP CONSTRAINT "FK_74abf75e6d975ab24659cc6b7c3"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "heroes" DROP CONSTRAINT "FK_febd0a5e4eac8e0b9c7cf6315d3"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "heroes" DROP CONSTRAINT "FK_2865df5ead1fb8f768a342676fd"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "powers"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "revoked_tokens"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "users"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TYPE "public"."users_role_enum"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "attributes"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "heroes"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TYPE "public"."heroes_status_enum"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "publishers"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "alignments"
         `);
-    }
-
+  }
 }
